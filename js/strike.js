b000ee17aac700f05968001db33fc8c644096c6f
grindpride <evil.koprof@gmail.com>
2017-02-19 19:42:59 +0500

    ranger add

--- js/strike.js
+++ js/strike.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log("Poehali!");
     var button = $('.button');
-    // var range = $('#range').val();
+    var range = 60000/($('#range').val());
     var t;
     button.click(function() {
         $(this).toggleClass('pressed');
@@ -17,12 +17,11 @@ $(document).ready(function() {
         var diod = $('.thisCol');
         var thisColumn = col.index($('.active'));
         var row = col.eq(thisColumn).children('.pressed');
-        for(var i = 0; i<row.length;i++){
+        for (var i = 0; i < row.length; i++) {
             var thisBeat = row.eq(i).index();
-             playBeat(thisBeat)
-             // console.log('В колонне'+thisColumn+'Нажаты кнопки №: ' + thisBeat);
+            playBeat(thisBeat);
         }
-       
+
         if (thisColumn === 15) {
             var nextColumn = 0;
         } else {
@@ -33,20 +32,25 @@ $(document).ready(function() {
         col.eq(nextColumn).addClass('active');
         diod.removeClass('active');
         diod.eq(nextColumn).addClass('active');
+        range = 60000/($('#range').val());
+        var t = setTimeout(nextStep, range);
+        $('.stop').on('click', function() {
+            console.log('STAPH!!');
+            clearTimeout(t);
+        });
     };
 
 
 
     $('.start').click(function() {
 
-        var t = setInterval(nextStep, 100);
-        
-        $('.stop').on('click', function() {
-            console.log('STAPH!!');
-            clearInterval(t);
+        var t = setTimeout(nextStep, range);
 
-        });
+
     });
+
+
+
     $('.clear').click(function() {
         var $rc = $('.button').removeClass('pressed');
     });
