70c8302c0fbf98461faca7e4a5d92c472ea487e3
grindpride <evil.koprof@gmail.com>
2018-06-24 17:16:20 +0300

    fixes

--- dist/06487297b2a70c7082d9bea6ee99e446.js
+++ dist/06487297b2a70c7082d9bea6ee99e446.js
@@ -1,3 +1,334 @@
-require=function(r,e,n){function t(n,o){function i(r){return t(i.resolve(r))}function f(e){return r[n][1][e]||e}if(!e[n]){if(!r[n]){var c="function"==typeof require&&require;if(!o&&c)return c(n,!0);if(u)return u(n,!0);var l=new Error("Cannot find module '"+n+"'");throw l.code="MODULE_NOT_FOUND",l}i.resolve=f;var s=e[n]=new t.Module(n);r[n][0].call(s.exports,i,s,s.exports)}return e[n].exports}function o(r){this.id=r,this.bundle=t,this.exports={}}var u="function"==typeof require&&require;t.isParcelRequire=!0,t.Module=o,t.modules=r,t.cache=e,t.parent=u;for(var i=0;i<n.length;i++)t(n[i]);return t}({165:[function(require,module,exports) {
-console.log("intro init");var e=!1,t=!1;"localhost:1234"===window.location.host&&(e=!0);var o=document.createElement("script");o.src="https://www.youtube.com/player_api";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(o,n);var i,a,c={autoplay:1,autohide:1,modestbranding:1,rel:0,showinfo:0,controls:0,disablekb:1,enablejsapi:1,iv_load_policy:3},l=[{videoId:"AzR_QYX1eEM",suggestedQuality:"hd720"}],r=Math.floor(Math.random()*l.length),d=r,s=27;function u(){e||t||i.loadVideoById(l[d])}function v(e){var t=$('<a href="#" id="fakeClick"></a>');t.bind("click",function(t){t.preventDefault(),e()}),$("body").append(t);var o,n=$("#fakeClick").get(0);document.createEvent&&(o=document.createEvent("MouseEvents")).initMouseEvent&&(o.initMouseEvent("click",!0,!0,window,0,0,0,0,0,!1,!1,!1,!1,0,null),n.dispatchEvent(o)),$(n).remove()}function p(e){var t,o;$("#tv").addClass("active"),clearTimeout(a),e.data==YT.PlayerState.PLAYING&&(t=i.getCurrentTime())+.4<s&&(o=i.getPlaybackRate(),a=setTimeout(y,1e3*((s-t)/o)))}function y(){i.pauseVideo()}function f(){var e=$(window).width()+200;$(window).height();i.setSize(e,e/16*9),$(".tv .screen").css({left:"0px"})}console.log(d),window.onYouTubePlayerAPIReady=function(){console.log("YouTubePlayerAPIReady"),t||(i=new YT.Player("tv",{events:{onReady:u,onStateChange:p},playerVars:c}))},$(window).on("load resize",function(){i&&f()}),$("#playIntro").click(function(){i&&(i.stopVideo(),i.playVideo())}),$("#skipIntro").click(function(){t=!0,$("section.section-intro").slideUp(300),i&&y()}),e&&$("#skipIntro").click();
-},{}]},{},[165])
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+require = (function (modules, cache, entry) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof require === "function" && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof require === "function" && require;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({3:[function(require,module,exports) {
+console.log('intro init');
+var debug = false;
+var skiped = false;
+if (window.location.host === 'localhost:1234') {
+    debug = true;
+}
+
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/player_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var tv,
+    playerDefaults = { autoplay: 1,
+    autohide: 1,
+    modestbranding: 1,
+    rel: 0,
+    showinfo: 0,
+    controls: 0,
+    disablekb: 1,
+    enablejsapi: 1,
+    iv_load_policy: 3 };
+var vid = [{ 'videoId': 'AzR_QYX1eEM', 'suggestedQuality': 'hd720' }],
+    randomVid = Math.floor(Math.random() * vid.length),
+    currVid = randomVid;
+var stopPlayAt = 27;
+var stopPlayTimer;
+
+console.log(currVid);
+
+window.onYouTubePlayerAPIReady = function () {
+    console.log('YouTubePlayerAPIReady');
+    if (!skiped) {
+        tv = new YT.Player('tv', {
+            events: { 'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange },
+            playerVars: playerDefaults
+        });
+    }
+};
+
+function onPlayerReady() {
+    if (!debug && !skiped) {
+        tv.loadVideoById(vid[currVid]);
+
+        // tv.playVideo();
+    }
+}
+function fakeClick(fn) {
+    var $a = $('<a href="#" id="fakeClick"></a>');
+    $a.bind("click", function (e) {
+        e.preventDefault();
+        fn();
+    });
+
+    $("body").append($a);
+
+    var evt,
+        el = $("#fakeClick").get(0);
+
+    if (document.createEvent) {
+        evt = document.createEvent("MouseEvents");
+        if (evt.initMouseEvent) {
+            evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            el.dispatchEvent(evt);
+        }
+    }
+
+    $(el).remove();
+}
+
+function onPlayerStateChange(event) {
+    var time, rate, remainingTime;
+    $('#tv').addClass('active');
+    clearTimeout(stopPlayTimer);
+    if (event.data == YT.PlayerState.PLAYING) {
+        time = tv.getCurrentTime();
+        // Add .4 of a second to the time in case it's close to the current time
+        // (The API kept returning ~9.7 when hitting play after stopping at 10s)
+        if (time + .4 < stopPlayAt) {
+            rate = tv.getPlaybackRate();
+            remainingTime = (stopPlayAt - time) / rate;
+            stopPlayTimer = setTimeout(pauseVideo, remainingTime * 1000);
+        }
+    }
+}
+function pauseVideo() {
+    tv.pauseVideo();
+}
+
+// function onPlayerStateChange(e) {
+//     if (e.data === 1){
+//         $('#tv').addClass('active');
+//         $('.hi em:nth-of-type(2)').html(currVid + 1);
+//     } else if (e.data === 2){
+//         $('#tv').removeClass('active');
+//         if(currVid === vid.length - 1){
+//             currVid = 0;
+//         } else {
+//             currVid++;
+//         }
+//         // tv.loadVideoById(vid[currVid]);
+//         // tv.seekTo(vid[currVid].startSeconds);
+//     }
+// }
+
+function vidRescale() {
+
+    var w = $(window).width() + 200,
+        h = $(window).height() + 200;
+
+    tv.setSize(w, w / 16 * 9);
+    $('.tv .screen').css({ 'left': '0px' });
+}
+
+$(window).on('load resize', function () {
+    if (tv) vidRescale();
+});
+
+$('#playIntro').click(function () {
+    if (tv) {
+        tv.stopVideo();
+        tv.playVideo();
+    }
+});
+
+$('#skipIntro').click(function () {
+    skiped = true;
+    $('section.section-intro').slideUp(300);
+    if (tv) {
+        pauseVideo();
+    }
+});
+
+if (debug) {
+    $('#skipIntro').click();
+}
+},{}],141:[function(require,module,exports) {
+
+var global = (1, eval)('this');
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    accept: function (fn) {
+      this._acceptCallback = fn || function () {};
+    },
+    dispose: function (fn) {
+      this._disposeCallback = fn;
+    }
+  };
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51255' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      data.assets.forEach(function (asset) {
+        hmrApply(global.require, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.require, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + 'data.error.stack');
+    }
+  };
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(+k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  if (cached && cached.hot._disposeCallback) {
+    cached.hot._disposeCallback();
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallback) {
+    cached.hot._acceptCallback();
+    return true;
+  }
+
+  return getParents(global.require, id).some(function (id) {
+    return hmrAccept(global.require, id);
+  });
+}
+},{}]},{},[141,3])
+//# sourceMappingURL=/dist/06487297b2a70c7082d9bea6ee99e446.map
--- dist/2426d10a4e20582852e537cc29871ebc.js
+++ dist/2426d10a4e20582852e537cc29871ebc.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({264:[function(require,module,exports) {
+})({128:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -101,7 +101,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],258:[function(require,module,exports) {
+},{}],109:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,37 +132,37 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":264}],259:[function(require,module,exports) {
+},{"./bundle-url":128}],111:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./..\\assets\\img\\player-01.png":265,"./..\\assets\\img\\player-02.png":266,"./..\\assets\\img\\player-03.png":267,"./..\\assets\\img\\player-04.png":268,"./..\\assets\\img\\player-05.png":271,"./..\\assets\\img\\player-06.png":269,"./..\\assets\\img\\player-07.png":270,"./..\\assets\\img\\player-08.png":272,"./..\\assets\\img\\player-09.png":273,"./..\\assets\\img\\player-10.png":274,"./..\\assets\\img\\player-11.png":275,"./..\\assets\\img\\player-12.png":276,"./..\\assets\\img\\ava\\arhan14.png":280,"./..\\assets\\img\\ava\\geek14.png":283,"./..\\assets\\img\\ava\\gvozd14.png":282,"./..\\assets\\img\\ava\\kadril14.png":281,"./..\\assets\\img\\ava\\krivonos14.png":287,"./..\\assets\\img\\ava\\ryabo14.png":284,"./..\\assets\\img\\ava\\steb14.png":285,"./..\\assets\\img\\ava\\strep14.png":286,"./..\\assets\\img\\sport.png":288,"./..\\assets\\img\\aim.png":289,"./..\\assets\\img\\fly.png":277,"./..\\assets\\img\\friend.png":278,"_css_loader":258}],260:[function(require,module,exports) {
+},{"./..\\assets\\img\\player-01.png":115,"./..\\assets\\img\\player-02.png":116,"./..\\assets\\img\\player-03.png":127,"./..\\assets\\img\\player-04.png":118,"./..\\assets\\img\\player-05.png":117,"./..\\assets\\img\\player-06.png":120,"./..\\assets\\img\\player-07.png":122,"./..\\assets\\img\\player-08.png":121,"./..\\assets\\img\\player-09.png":123,"./..\\assets\\img\\player-10.png":124,"./..\\assets\\img\\player-11.png":126,"./..\\assets\\img\\player-12.png":125,"./..\\assets\\img\\ava\\arhan14.png":132,"./..\\assets\\img\\ava\\geek14.png":133,"./..\\assets\\img\\ava\\gvozd14.png":134,"./..\\assets\\img\\ava\\kadril14.png":136,"./..\\assets\\img\\ava\\krivonos14.png":137,"./..\\assets\\img\\ava\\ryabo14.png":138,"./..\\assets\\img\\ava\\steb14.png":139,"./..\\assets\\img\\ava\\strep14.png":140,"./..\\assets\\img\\sport.png":129,"./..\\assets\\img\\aim.png":130,"./..\\assets\\img\\fly.png":135,"./..\\assets\\img\\friend.png":131,"_css_loader":109}],110:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./..\\assets\\img\\romb.png":279,"_css_loader":258}],261:[function(require,module,exports) {
+},{"./..\\assets\\img\\romb.png":119,"_css_loader":109}],112:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":258}],262:[function(require,module,exports) {
+},{"_css_loader":109}],113:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":258}],156:[function(require,module,exports) {
+},{"_css_loader":109}],2:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./field.pcss":259,"./aside.pcss":260,"./content.pcss":261,"./normalize.css":262,"_css_loader":258}],301:[function(require,module,exports) {
+},{"./field.pcss":111,"./aside.pcss":110,"./content.pcss":112,"./normalize.css":113,"_css_loader":109}],141:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -184,7 +184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62704' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51255' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -285,7 +285,7 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}],303:[function(require,module,exports) {
+},{}],143:[function(require,module,exports) {
 var getBundleURL = require('./bundle-url').getBundleURL;
 
 function loadBundlesLazy(bundles) {
@@ -364,7 +364,7 @@ LazyPromise.prototype.then = function (onSuccess, onError) {
 LazyPromise.prototype.catch = function (onError) {
   return this.promise || (this.promise = new Promise(this.executor).catch(onError));
 };
-},{"./bundle-url":264}],0:[function(require,module,exports) {
-var b=require(303);b.load([["a74391ee941c675d47547fbc127c9953.png",265],["012129dfa8b9697761b3dd8be4c0d093.png",266],["26912646c726da07792372becae14488.png",267],["2109065c09b3c9e14828a01a55364146.png",268],["c5a4e2268e29659f3b62f9043881e3ba.png",271],["0fdb78fb9d861d03c3902604792ea911.png",269],["78452f0975ceaaab6893b3a2b86410a7.png",270],["0da5f15327975923990c3320b11f3da1.png",272],["70869026b8cd2047eca4ccb0a26e0524.png",273],["d33b1283ff53d7f993eb42af961971a2.png",274],["5c8ba66099f30cbaf14f0c4261b76f34.png",275],["783cc6ac086352342f5e976d1806d875.png",276],["73370bf648397b94e3cab14f95d9f941.png",280],["7055838e83b9df068c196f198110f02e.png",283],["f0d511bf12011d9b4dab0f44d6d5e30d.png",282],["c8918be054856bcc5f3c3c9401ea420d.png",281],["8bb9c6b646a21949970cb3ad9df4ce6a.png",287],["313158ec61e2fa80294a4bab5e3840e2.png",284],["d7204d279a780aaf32f80b21905524bb.png",285],["c57fc2949180e8218569573eaeb66400.png",286],["2afb49cc4dc79d272196ddf4e6b0cd36.png",288],["8fc7e6f85a4b07cba11430890a5ba67f.png",289],["52f13e67d50accbe04d851a9e5f2862f.png",277],["d4446dadf14d11f2aa1a4fbc8280d294.png",278],["7d8a70c9b1c2b930f6130a66592ecdf5.png",279]]);
-},{}]},{},[301,0])
+},{"./bundle-url":128}],0:[function(require,module,exports) {
+var b=require(143);b.load([["a74391ee941c675d47547fbc127c9953.png",115],["012129dfa8b9697761b3dd8be4c0d093.png",116],["26912646c726da07792372becae14488.png",127],["2109065c09b3c9e14828a01a55364146.png",118],["c5a4e2268e29659f3b62f9043881e3ba.png",117],["0fdb78fb9d861d03c3902604792ea911.png",120],["78452f0975ceaaab6893b3a2b86410a7.png",122],["0da5f15327975923990c3320b11f3da1.png",121],["70869026b8cd2047eca4ccb0a26e0524.png",123],["d33b1283ff53d7f993eb42af961971a2.png",124],["5c8ba66099f30cbaf14f0c4261b76f34.png",126],["783cc6ac086352342f5e976d1806d875.png",125],["73370bf648397b94e3cab14f95d9f941.png",132],["7055838e83b9df068c196f198110f02e.png",133],["f0d511bf12011d9b4dab0f44d6d5e30d.png",134],["c8918be054856bcc5f3c3c9401ea420d.png",136],["8bb9c6b646a21949970cb3ad9df4ce6a.png",137],["313158ec61e2fa80294a4bab5e3840e2.png",138],["d7204d279a780aaf32f80b21905524bb.png",139],["c57fc2949180e8218569573eaeb66400.png",140],["2afb49cc4dc79d272196ddf4e6b0cd36.png",129],["8fc7e6f85a4b07cba11430890a5ba67f.png",130],["52f13e67d50accbe04d851a9e5f2862f.png",135],["d4446dadf14d11f2aa1a4fbc8280d294.png",131],["7d8a70c9b1c2b930f6130a66592ecdf5.png",119]]);
+},{}]},{},[141,0])
 //# sourceMappingURL=/dist/2426d10a4e20582852e537cc29871ebc.map
